a4d09d81b1427f7e459be38fb4ad43107ca0fba8
blackMountain <uquantum@hotmail.com>
2021-05-30 14:45:09 +0800

    compare to 的用法

--- src/String/s5.java
+++ src/String/s5.java
@@ -3,6 +3,17 @@ package String;
 // equal if they are the same length and contain the same characters in the same positions.
 public class s5 {
     public static void main(String[] args) {
+        String str1= "the String1";
+        String str2= "the String2";
+        int equals=str1.compareTo(str2);
+        System.out.println(equals);
+        if (equals>0){
+            System.out.println(str1+" is larger than "+str2);
+        }else if (equals==0){
+            System.out.println(str1+" is equal to "+str2);
+        }else{
+            System.out.println(str1+" is less than "+str2);
+        }
 
     }
 }
